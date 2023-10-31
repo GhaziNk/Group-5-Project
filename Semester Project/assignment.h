15078e32fed0818ed3113896dc427f3793b823ee
MrMiu2 <98138153+MrMiu2@users.noreply.github.com>
2023-10-30 22:17:56 -0700

    Added Constructors

--- Semester Project/assignment.h
+++ Semester Project/assignment.h
@@ -4,9 +4,8 @@ using namespace std;
 class Assignment {
 public:
     Assignment();
-    Assignment (string name, string username, string desc);
+    Assignment (string name, string username, string desc, int priority);
     ~Assignment();
-
     string GetAssignmentName();
 
 
@@ -18,3 +17,18 @@ private:
 
 
 };
+
+//dunno if this is really necessary, but just in case
+Assignment::Assignment() {
+    assignmentName = "New";
+    assignmentDesc = "New desc";
+    assignedUserName = "MiuWasHere";
+    priorityLevel = 0;
+}
+
+Assignment::Assignment(string name, string username, string desc, int priority) {
+    assignmentName = name;
+    assignmentDesc = desc;
+    assignedUserName = username;
+    priorityLevel = priority;
+}
